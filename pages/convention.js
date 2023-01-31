08b1acb9f83e4f22fb245a6106f242db8851c3d8
Adnene_wsl <adnen.guessoum@gmail.com>
2023-01-31 23:09:58 +0100

    Add outline convention page

---
+++ pages/convention.js
@@ -0,0 +1,169 @@
+import Section from '../components/section';
+import { Paragraph, Titre } from '../components/paragraph';
+import {
+  Flex,
+  Image,
+  Text,
+  Container,
+  Divider,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+const PageAtouts = () => {
+  return (
+    <Flex direction="column" w="75%" ml="280px">
+      <Container>
+        <Image src="/images/convention/titre_convention.gif" alt="bannière convention" />
+        <Divider bg="black" mt={18} mb={18} />
+      </Container>
+
+      <Section delay={0.1}>
+        <Flex gap={3} align="center">
+          <Image
+            src="/images/convention/convention.gif"
+            alt="illustration convention"
+            h="120px"
+          />
+          <Titre>
+		La convention de services est signée par le réparateur et par ASSERCAR, elle engage également les assureurs cités qui s&apos;efforcent d&apos;orienter leurs clients/assurés vers les réparateurs signataires. Chaque partie retire des avantages de la convention :
+		</Titre>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={0.2}>
+        <Flex gap={4} align="start">
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+						m={5}
+          />
+          <Paragraph>
+            Une convention de services motivante pour toutes les parties et qui
+            tend essentiellement à la satisfaction du client/assuré.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={0.4}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>
+            Une excellente connaissance du terrain et des réparateurs dans
+            toutes les régions de France, grâce à son partenaire
+            <Text as="span" ml={2} decoration="underline">
+              <NextLink href="/assureur">BCA Expertise</NextLink>
+            </Text>
+            , importante entreprise d&apos;expertise en automobile.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={0.6}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>
+            La mise à disposition de ses clients d&apos;un système
+            <Text as="span" m={2} decoration="underline">
+              <NextLink href="/cartographie">cartographique</NextLink>
+            </Text>
+            de géolocalisation pour une recherche rapide des réparateurs proches
+            du client/assuré.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={0.8}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>
+            Un suivi précis de l&apos;activité des réparateurs grâce à des
+            liaisons informatiques permanentes avec nos clients et partenaires.
+            Les réparateurs obtiennent une réponse immédiate à toute question
+            concernant les dossiers et les règlements, en cours ou déjà
+            effectués.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={1}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>
+            La fourniture à nos clients de statistiques hebdomadaires (dès le
+            lundi suivant) avec une synthèse de l&apos;activité et les nombres
+            de dossiers et taux d&apos;orientation par région, par entité et
+            même par gestionnaire sinistre. Ces statistiques constituent un
+            véritable outil de management et de motivation de tous les
+            intervenants.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={1.2}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>
+            Un rapport mensuel d&apos;activité pour chaque client/assureur (au
+            plus tard le 3ème jour ouvrable du mois suivant) avec la situation
+            du réseau et les résultats chiffrés de l&apos;activité.
+          </Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+
+      <Section delay={1.4}>
+        <Flex gap={4}>
+          <Image
+            src="/images/atouts/puce.gif"
+            alt="puce"
+            w="15px"
+            h="15px"
+            m={5}
+          />
+          <Paragraph>Un coût de fonctionnement très faible.</Paragraph>
+        </Flex>
+        <Divider bg="black" mt={18} mb={18} />
+      </Section>
+    </Flex>
+  );
+};
+
+export default PageAtouts;
---
+++ pages/recrutement.js
@@ -0,0 +1,82 @@
+import Section from '../components/section';
+import { Paragraph, Titre } from '../components/paragraph';
+import {
+	Flex,
+	Image,
+	Text,
+	Container,
+	Divider,
+} from '@chakra-ui/react';
+
+import {
+	TableContainer,
+	TableCaption,
+	Table,
+	Thead,
+	Tbody,
+	Tfoot,
+	Th,
+	Tr,
+	Td
+} from '@chakra-ui/react';
+
+const PageRecrutement = () => {
+	return (
+		<Flex direction="column" w="75%" ml="280px">
+		<Container>
+		<Image src="/images/recrutement/logo_recrutement.png" alt="bannière recrutement" />
+		</Container>
+		<Divider bg="black" mt={18} mb={18} />
+		<Titre as="h1">ASSERCAR recrute :</Titre>
+		<Section delay={0.2}>
+		<Text color="black" mt={2} style={{ textIndent: 20 }}>
+		ASSERCAR, assure depuis 2004 la constitution et l&apos;animation d&apos;un réseau qui approche aujourd&apos;hui 1 800 garages agrés sur l&apos;ensemble du territoire national.
+		Avec 250 000 véhicules réparés dans le réseau en 2018, ASSERCAR est un acteur majeur du secteur de la réparation automobile. C&apos;est plus de 350 Millions d&apos;Euros de réparations qui ont transité dans les ateliers partenaires d&apos;ASSERCAR.
+		</Text>
+
+		</Section>
+		<Section delay={0.4}>
+		<Text color="black" mt={2} style={{ textIndent: 20 }}>
+		Pour l&apos;ensemble de ses clients (assureurs, courtiers, mutuelles, gestionnaires de flottes,...), ASSERCAR constitue un important levier de performance, tant pour la maîtrise des coûts techniques que pour la satisfaction et la fidélisation des assurés. Confiance, flexibilité, réactivité, partenariat équitable et durable, sont les valeurs qui animent notre quotidien
+		</Text>
+		</Section>
+
+		<Section delay={0.6}>
+		<Text color="black" mt={2} style={{ textIndent: 20 }}>
+		Inscrit dans une forte dynamique de croissance, ASSERCAR recrute aujourd&apos;hui pour conduire des projets innovants et structurants.
+		</Text>
+		</Section>
+
+		<Divider bg="black" mt={18} mb={18} />
+
+		<Section delay={0.4}>
+<TableContainer border=" solid 1px black">
+  <Table variant='simple' >
+    <TableCaption color="black" bg="lightgrey">Offres d&apos;emplois disponibles</TableCaption>
+    <Thead>
+      <Tr bg="red">
+        <Th>Contrat</Th>
+        <Th>Intitulé de l&apos;offre</Th>
+        <Th>Métier</Th>
+        <Th>Localisation</Th>
+        <Th>Date</Th>
+      </Tr>
+    </Thead>
+    <Tbody color="black" border="solid black 1px">
+      <Tr></Tr>
+      <Tr></Tr>
+      <Tr></Tr>
+    </Tbody>
+    <Tfoot></Tfoot>
+  </Table>
+</TableContainer>
+		</Section>
+
+		<Divider bg="black" mt={18} mb={18} />
+
+		</Flex>
+	)
+};
+
+export default PageRecrutement;
+
